58e38292f201029bd8378aaf74c632ac29f5fa42
franwe <franziska.wehrmann@gmail.com>
2021-04-05 14:45:26 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="locla_polynomial_regression",
+    version="0.0.1",
+    url="https://github.com/franwe/locla_polynomial_regression",
+    author="Franziska Wehrmann",
+    author_email="franziska.wehrmann@gmail.com",
+    description="Performs local polynomial regression. Returns the fit and its first and second derivative.",
+    packages=find_packages(),
+    install_requires=["numpy >= 1.11.1"],
+)
