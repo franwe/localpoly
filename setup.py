8252241034ecea2b37cf6dd99a26f47b7d0e1f83
franwe <franziska.wehrmann@gmail.com>
2021-04-18 18:06:23 +0300

    change requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url="https://github.com/franwe/localpoly",
     author="Franziska Wehrmann",
     author_email="franziska.wehrmann@gmail.com",
-    description="Performs local polynomial regression. Returns the fit and its first and second derivative.",
+    description="Performs local polynomial regression. Returns the fit and its first and second derivative. Includes cross validation for bandwidth optimization",
     packages=find_packages(),
-    install_requires=["numpy >= 1.11.1", "pandas", "pydantic", "strictyaml", "scipy"],
+    install_requires=["numpy >= 1.11.1", "pandas >= 1.2.4", "scipy >= 1.6.2"],
 )
