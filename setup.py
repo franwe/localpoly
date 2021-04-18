d1efb889da1dc037a330266169ee19737a0543fd
franwe <franziska.wehrmann@gmail.com>
2021-04-18 18:28:13 +0300

    add more setup info

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="localpoly",
-    version="0.0.1",
+    version="0.1.0",
     url="https://github.com/franwe/localpoly",
     author="Franziska Wehrmann",
     author_email="franziska.wehrmann@gmail.com",
-    description="Performs local polynomial regression. Returns the fit and its first and second derivative. Includes cross validation for bandwidth optimization",
-    packages=find_packages(),
+    description="Performs Local Polynomial Regression.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["localpoly"],
     install_requires=["numpy >= 1.11.1", "pandas >= 1.2.4", "scipy >= 1.6.2"],
 )
