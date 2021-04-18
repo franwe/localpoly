525292ad8b9761a56fda4b45dd54bd1c6ef083bb
franwe <franziska.wehrmann@gmail.com>
2021-04-18 19:05:08 +0300

    add additional setup.py setup info

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     version="0.1.0",
     url="https://github.com/franwe/localpoly",
     project_urls={
-        Documentation = "https://localpoly.readthedocs.io/en/latest/",
-        Code = "https://github.com/franwe/localpoly"
+        "Documentation" : "https://localpoly.readthedocs.io/en/latest/",
+        "Code" : "https://github.com/franwe/localpoly"
     }
     author="Franziska Wehrmann",
     author_email="franziska.wehrmann@gmail.com",
