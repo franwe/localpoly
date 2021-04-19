74bc828a6267c381fb84c1d8f4eacfc633f12842
franwe <franziska.wehrmann@gmail.com>
2021-04-19 11:26:04 +0300

    docs: change root path?

--- docs/conf.py
+++ docs/conf.py
@@ -22,11 +22,14 @@ version = "0.0.12"
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(inspect.getfile(inspect.currentframe())))
 
+print("my location: ", __location__)
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("."))
+# sys.path.insert(0, os.path.abspath("."))
 
+sys.path.insert(0, os.path.abspath('./../'))
+autodoc_mock_imports = ["utils"]
 
 # -- Run sphinx-apidoc ------------------------------------------------------
 # This hack is necessary since RTD does not issue `sphinx-apidoc` before running
